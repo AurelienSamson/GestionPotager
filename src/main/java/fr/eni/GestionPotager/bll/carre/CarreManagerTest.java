0074b807963ca627fbc9f3baa3199a45d3b0443e
Aurélien Samson <aurelien2.samson@gmail.com>
2021-10-21 15:34:43 +0200

    ADD #aurelien 2 : ajout classe test carré

---
+++ src/main/java/fr/eni/GestionPotager/bll/carre/CarreManagerTest.java
@@ -0,0 +1,30 @@
+package fr.eni.GestionPotager.bll.carre;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+import fr.eni.GestionPotager.bll.potager.PotagerManager;
+import fr.eni.GestionPotager.bo.Carre;
+import fr.eni.GestionPotager.bo.Potager;
+
+public class CarreManagerTest implements CommandLineRunner {
+	
+	@Autowired
+	private PotagerManager managerPotager;
+	
+	@Autowired
+	private CarreManager managerCarre;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Potager potager = new Potager("2 bis avenue de la gare", "La Jardinière", 100, "Quimper");
+		
+		Carre carre = new Carre(25, "humide", "Soleil");
+	}
+
+}
