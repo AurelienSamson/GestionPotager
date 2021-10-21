1e1caf8779a05a6492d911e1b140a3ef1c8b6b36
Romuald29 <romuald.gagnon2021@campus-eni.fr>
2021-10-21 13:29:31 +0200

    ADD bll potager

--- src/main/java/fr/eni/GestionPotager/bll/potager/PotagerManager.java
+++ src/main/java/fr/eni/GestionPotager/bll/potager/PotagerManager.java
@@ -1,5 +1,11 @@
 package fr.eni.GestionPotager.bll.potager;
 
+import java.util.List;
+
+import fr.eni.GestionPotager.bo.Potager;
+
 public interface PotagerManager {
 
+	public void addPotager(Potager potager);
+	public List<Potager> getAllPotager();
 }
--- src/main/java/fr/eni/GestionPotager/bll/potager/PotagerManagerImpl.java
+++ src/main/java/fr/eni/GestionPotager/bll/potager/PotagerManagerImpl.java
@@ -1,5 +1,25 @@
 package fr.eni.GestionPotager.bll.potager;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.eni.GestionPotager.bo.Potager;
+import fr.eni.GestionPotager.dal.PotagerDAO;
+
 public class PotagerManagerImpl implements PotagerManager {
+
+	@Autowired
+	PotagerDAO dao;
+	@Override
+	public void addPotager(Potager potager) {
+		dao.save(potager);
+	}
+
+	@Override
+	public List<Potager> getAllPotager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
